4d94f225c6cd2fdba52235bff421675a4af40bfb
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-29 20:45:09 -0600

    Ejercicio 3 terminado - eventos

--- scripts/Ejercicio3.js
+++ scripts/Ejercicio3.js
@@ -1,1 +1,19 @@
 console.log("Ejercicio 3");
+
+function accionBotonUno() {
+  alert("Haz hecho click en el botón 1, por eso estoy saliendo en tu pantalla");
+}
+
+function accionBotonDos() {
+  alert(
+    "Haz hecho docle click en el botón 2, por eso estoy saliendo en tu pantalla"
+  );
+}
+
+function accionBotonTres() {
+  alert(
+    "Haz pasado tu curso sobre el botón 3, por eso estoy saliendo en tu pantalla"
+  );
+  document.getElementById("btn-3").innerText = "Ya pasaste el mouse";
+  console.log("Puedes hacer más cosas con eventos :D");
+}
