9d835914e8a6bd20c32de500c81676f32dacf5d2
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-29 20:12:30 -0600

    Ejercicio 2 terminado

--- scripts/Ejercicio2.js
+++ scripts/Ejercicio2.js
@@ -1,1 +1,5 @@
 console.log("Ejercicio 2");
+const rectangulo = document.querySelector(".rectangulo");
+rectangulo.style.backgroundColor = "#00E8DB";
+
+/* document.querySelector(".rectangulo").style.backgroundColor = "#00E8DB"; */
